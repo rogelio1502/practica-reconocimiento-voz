a6ad01572a70dfc757e60cad7b9b3f15d1bfe156
rogelio1502 <55726045+rogelio1502@users.noreply.github.com>
2021-03-24 12:44:11 -0600

    Delete salida-voz.py

--- salida-voz.py
+++
@@ -1,8 +0,0 @@
-from gtts import gTTS
-from playsound import playsound
-voz = gTTS("Hola perros",lang="es-us")
-NOMBRE_ARCHIVO="texto2.mp3"
-with open(NOMBRE_ARCHIVO,'wb') as archivo:
-    voz.write_to_fp(archivo)
-
-playsound(NOMBRE_ARCHIVO)
